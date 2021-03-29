2a22a19e812e2eaae6047a1ccbe67da1dde11497
Wellingtonsc66 <wellingtonsc66@gmail.com>
2021-03-28 21:00:36 -0300

    Atualizando rotas

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,12 @@ Route::redirect('/', '/login');
 
 Route::group(['prefix'=>'dashboard', 'namespace'=>'office', 'middleware'=>'auth'], function (){
     Route::get('/', function () {
-        return view('dashboard');
+        $produtos = (new \App\Models\Produto())->all()->toArray();
+        return view('dashboard', compact('produtos'));
     })->name('dashboard');
 
-    Route::get('/estoque/adicionar', [EstoqueController::class, 'adicionar'])->name('estoque.adicionar');
-    Route::get('/estoque/baixar', [EstoqueController::class, 'baixar'])->name('estoque.baixar');
+    Route::match(['get', 'post'],'/estoque/adicionar', [EstoqueController::class, 'adicionar'])->name('estoque.adicionar');
+    Route::match(['get', 'post'],'/estoque/baixar', [EstoqueController::class, 'baixar'])->name('estoque.baixar');
     Route::get('/estoque/relatorio', [EstoqueController::class, 'relatorio'])->name('estoque.relatorio');
 });
 
